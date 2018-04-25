2220472ca579a6be57ae2c809f6704bd034f73e2
cscov <carolyn.l.scoville@gmail.com>
2018-04-25 02:09:28 -0700

    Inital Create Account form styling

---
+++ src/components/create_account.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from './header';
+import Field from './field';
+import Button from './button';
+
+class CreateAccount extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const { containerStyle, headerTextStyle, redButtonStyle,
+    whiteTextStyle } = styles;
+    return (
+      <View>
+      <Header title="Create Account" headerTextStyle={headerTextStyle}
+      containerStyle={containerStyle}/>
+      <Field label="First Name" placeholder="First Name" />
+      <Field label="Last Name" placeholder="Last Name" />
+      <Field label="Phone Number" placeholder="Phone Number" />
+      <Field label="Password" placeholder="Password" />
+      <Field label="Confirm Password" placeholder="Confirm Password" />
+
+      <Button buttonStyle={redButtonStyle} textStyle={whiteTextStyle}>
+        Create Account
+      </Button>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  redButtonStyle: {
+    height: 60,
+    borderRadius: 5,
+    borderWidth: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: "#ed4008",
+    borderColor: "#ed4008",
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+
+  whiteTextStyle: {
+    alignSelf: 'center',
+    marginLeft: 45,
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+
+  headerTextStyle: {
+    color: '#ed4008',
+    fontSize: 22,
+    fontWeight: '600',
+  },
+
+  containerStyle: {
+    paddingTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 80,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+  },
+};
+
+export default CreateAccount;
