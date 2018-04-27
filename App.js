1aa5bcee71429d9cc8e88fe9194014e218e3d37b
Yangchen Shen <shyangch@gmail.com>
2018-04-27 16:26:21 -0700

    Put content into App and import App to index

--- App.js
+++ App.js
@@ -1,3 +1,78 @@
+import React from 'react';
+import {Platform, SafeAreaView, View } from 'react-native';
+import { Navigation, Screen } from 'react-native-navigation';
+import {registerScreens, registerScreenVisibilityListener} from './src/screens';
+import {Provider} from 'react-redux';
+import configureStore from './src/store/configureStore';
+import firebase from 'react-native-firebase';
+
+const store = configureStore();
+
+registerScreens(store, Provider);
+registerScreenVisibilityListener();
+
+const tabs = [{
+  label: 'Pantry',
+  screen: 'pcnk.Pantry',
+  icon: require('./assets/nav_icons/pcnk_icon_only_red_half_size.png'),
+  title: 'Pantry',
+}, {
+  label: 'Map',
+  screen: 'pcnk.Map',
+  icon: require('./assets/nav_icons/map_red_half_size.png'),
+  title: 'Map',
+}, {
+  label: 'Help',
+  screen: 'pcnk.Help',
+  icon: require('./assets/nav_icons/help_red_half_size.png'),
+  title: 'Help',
+}, {
+  label: 'UserProfile',
+  screen: 'pcnk.UserProfile',
+  icon: require('./assets/nav_icons/user_red_half_size.png'),
+  title: 'UserProfile',
+}];
+
+Navigation.startTabBasedApp({
+  tabs,
+  animationType: Platform.OS === 'ios' ? 'slide-down' : 'fade',
+  tabsStyle: {
+    tabBarBackgroundColor: '#61D2D0',
+    tabBarButtonColor: '#ffffff',
+    tabBarSelectedButtonColor: '#ff505c',
+    tabFontFamily: 'BioRhyme-Bold',
+  },
+  appStyle: {
+    tabBarBackgroundColor: '#003a66',
+    navBarButtonColor: '#ffffff',
+    tabBarButtonColor: '#ffffff',
+    navBarTextColor: '#ffffff',
+    tabBarSelectedButtonColor: '#ed4008',
+    navigationBarColor: '#003a66',
+    navBarBackgroundColor: '#61D2D0',
+    statusBarColor: '#002b4c',
+    tabFontFamily: 'BioRhyme-Bold',
+  },
+  iconStyle: {
+    height: 30,
+    width: 30,
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: 'center',
+  }
+})
+
+
+// Navigation.showModal({
+//   screen: 'pcnk.SignIn', // unique ID registered with Navigation.registerScreen
+//   title: 'Modal', // title of the screen as appears in the nav bar (optional)
+//   passProps: {}, // simple serializable object that will pass as props to the modal (optional)
+//   navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
+//   navigatorButtons: {}, // override the nav buttons for the screen, see "Adding buttons to the navigator" below (optional)
+//   animationType: 'slide-up' // 'none' / 'slide-up' , appear animation for the modal (optional, default 'slide-up')
+// });
+
+
 // import { View, SafeAreaView } from 'react-native';
 // import StatusBarComponent from './src/components/status_bar'; // always present
 // import Pantry from './src/components/pantry';
