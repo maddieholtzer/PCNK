eb5fa5ac6e7f72f79a4ed1a3c111a4235333e673
Yangchen Shen <shyangch@gmail.com>
2018-04-27 11:16:55 -0700

    start redux file tree

---
+++ src/actions/auth.js
@@ -0,0 +1,20 @@
+'use strict'
+
+export const USER_LOGGED_IN = 'USER_LOGGED_IN'
+export const USER_LOGGED_OUT = 'USER_LOGGED_OUT'
+
+
+export function userLoggedIn( user ) {
+
+  return {
+  	user: user,
+    type: USER_LOGGED_IN
+  }
+}
+
+export function userLoggedOut(profileData) {
+
+  return {
+    type: USER_LOGGED_OUT,
+  }
+}
