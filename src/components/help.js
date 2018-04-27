8df4b3db89926e74d7389a95b2749a3278425929
Mattie <mattieholtzer@gmail.com>
2018-04-27 16:57:07 -0700

    pnatry and help pages styled

--- src/components/help.js
+++ src/components/help.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 const Help = () => {
+  const { containerStyle, headerStyle, contentStyle } = styles;
   return (
-    <View>
-      <Text>"You're in the help"</Text>
+    <View style={{flex:1, backgroundColor:'#FFD6C0'}}>
+      <ScrollView style={containerStyle}>
+        <Text style={headerStyle}>How It Works</Text>
+        <Text style={contentStyle}>Blah Blah Blah we have a cool app here is how to use it</Text>
+        <Text style={headerStyle}>About Us</Text>
+        <Text style={contentStyle}>Blah Blah Blah we have a cool app here is why we made it</Text>
+        <Text style={headerStyle}>Contact The Team</Text>
+        <Text style={contentStyle}>Blah Blah Blah we have a cool app we are cool pls hire us</Text>
+      </ScrollView>
     </View>
   );
+};
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+  },
+
+  headerStyle: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#333',
+    alignSelf: 'center',
+    textDecorationLine: 'underline'
+  },
+  contentStyle: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: '300',
+    color: '#333',
+    marginLeft: 25,
+    marginRight: 25
+  }
 };
 
 export default Help;
--- src/components/pantry.js
+++ src/components/pantry.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 const Pantry = () => {
+  const { backgroundStyle } = styles;
+  // var images = [],
+  //   imgWidth = 7,
+  //   winWidth =Dimensions.get('window').width;
+  //
+  //   for(var i=0;i<Math.ceil(winWidth / imgWidth);i++){
+  //       images.push((
+  //          <Image source={{uri: 'http://xxx.png'}} style={} />
+  //       ))
+  //   }
   return (
-    <View>
-      <Text>"You're in the pantry"</Text>
+    <View style={{flex: 1}}>
+      <Image source={require('../../assets/pantry-background.jpeg')}
+        style={backgroundStyle}/>
     </View>
   );
+};
+
+const styles = {
+  backgroundStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    width: undefined,
+    height: undefined,
+    // resizeMode: "contain",
+  },
 };
 
 export default Pantry;
