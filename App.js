e247157e4b254fec14fe0b25afaeade9b623a8be
Yangchen Shen <shyangch@gmail.com>
2018-04-29 22:22:11 -0700

    write user profile, change tab icon & text color

--- App.js
+++ App.js
@@ -20,10 +20,10 @@ export class App extends React.Component {
 
   onStoreUpdate() {
       const {root} = store.getState().root;
-      
+
       if (this.currentRoot != root) {
         this.currentRoot = root;
-        this.startApp(root);      
+        this.startApp(root);
       }
     }
 
@@ -56,9 +56,10 @@ export class App extends React.Component {
           animationType: Platform.OS === 'ios' ? 'slide-down' : 'fade',
           tabsStyle: {
             tabBarBackgroundColor: '#F9F9F9',
-            tabBarButtonColor: '#323031',
-            tabBarTextColor: '#323031',
+            tabBarButtonColor: '#595959',
+            tabBarTextColor: '#595959',
             tabBarSelectedButtonColor: '#ED4009',
+            tabBarSelectedTextColor: '#ED4009',
             tabFontFamily: 'BioRhyme-Bold',
           },
           appStyle: {
@@ -95,7 +96,7 @@ export class App extends React.Component {
         });
         break;
       }
-      
+
       case 'splash': {
         Navigation.startSingleScreenApp({
           screen: {
--- src/components/user_profile/user_profile.js
+++ src/components/user_profile/user_profile.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { store } from '../../../App';
+import { connect } from 'react-redux';
 
-export default class UserProfile extends React.Component {
+export class UserProfile extends React.Component {
   render() {
-    const { viewStyle } = styles;
-    const bio = store.getState().profile.bio;
-    const imgUrl = bio.picture.data.url;
-    console.log(store.getState().auth.currentUser);
-
+    const {
+      viewStyle,
+      avatarStyle,
+      smallTextStyle,
+      avatarWrapperStyle,
+      bigTextStyle,
+      headerContainerStyle
+    } = styles;
+    const currentUser = store.getState().auth.currentUser;
+    const imgUrl = currentUser.additionalUserInfo.profile.picture.data.url;
+    // TODO: replace groups and items with user info from state
+    const groups = 2;
+    const items = 5;
+    const level = Math.floor((groups*10 + items*5)/10);
     return (
       <View style={viewStyle}>
-        
+        <View style={headerContainerStyle}>
+          <View style={avatarWrapperStyle}>
+            <Image
+              style={avatarStyle}
+              source={{uri: `${imgUrl}`}}
+            />
+            <Text style={smallTextStyle}>{`Level ${level}`}</Text>
+          </View>
+
+          <View style={avatarWrapperStyle}>
+            <Text style={bigTextStyle}>{groups}</Text>
+            <Text style={smallTextStyle}>Groups</Text>
+          </View>
+
+          <View style={avatarWrapperStyle}>
+            <Text style={bigTextStyle}>{items}</Text>
+            <Text style={smallTextStyle}>Items Traded</Text>
+          </View>
+        </View>
+
       </View>
     );
   }
@@ -26,5 +55,28 @@ const styles = {
     borderTopWidth: 0.5,
     borderColor: '#A3A1A2',
     backgroundColor: "#ffffff",
+  },
+  avatarStyle: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  smallTextStyle: {
+    fontSize: 16,
+  },
+  avatarWrapperStyle: {
+    width: 100,
+    height: 130,
+    marginTop: 20,
+    marginLeft: 20,
+    alignItems: 'center',
+  },
+  bigTextStyle: {
+    fontSize: 20
+  },
+  headerContainerStyle: {
+    flexDirection: 'row'
   }
 };
+
+export default connect()(UserProfile);
