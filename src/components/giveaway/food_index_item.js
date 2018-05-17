8e2067df509fc5f464ee17349588c2214a17201d
cscov <carolyn.l.scoville@gmail.com>
2018-05-16 17:02:43 -0700

    Initial styling for food index item

--- src/components/giveaway/food_index_item.js
+++ src/components/giveaway/food_index_item.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import CardSection from '../card_section';
+import { View, Image } from 'react-native';
 
-const FoodIndexItem = () => {};
+const FoodIndexItem = ({item}) => {
+
+  return (
+    <CardSection>
+      <View>
+        <Image source={{uri: item.imgURL}} style={styles.thumbnailStyle}/>
+      </View>
+    </CardSection>
+  );
+};
+
+const styles = {
+  thumbnailStyle: {
+    height: 100,
+    width: 100,
+  },
+};
 
 export default FoodIndexItem;
